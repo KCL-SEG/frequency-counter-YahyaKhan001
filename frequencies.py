442b67410a0fce67e061a4349b7a4b535881fb40
YahyaKhan001 <102700231+YahyaKhan001@users.noreply.github.com>
2022-12-01 20:08:43 +0000

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,11 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    frequencies={}
+    for a in items: 
+        a=str(a)
+        if a in frequencies.keys(): 
+            frequencies[a]+=1
+        else:
+            frequencies[a]=1 
     return frequencies
